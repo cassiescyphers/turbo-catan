e4a3a14c74cff541b12d708a70b927d87ca45a60
David Daigle <david.t.daigle@hotmail.com>
2021-12-04 22:52:37 -0800

    fixed really weird roll tile distributions by using a "bag of numbers" approach

--- turbo_catan/generate.py
+++ turbo_catan/generate.py
@@ -15,7 +15,10 @@ class CatanBoard:
         
 
     def _gen_config(self,enable_bonus_rolls):
-        self.target_avg_roll_probability = 3.0
+        
+        # probability config
+        self.roll_lower_bound_pct = 0.75
+        self.roll_upper_bound_pct = 1.25
         
         # 3-4 players, 19 tiles
         # 5-6 players, 30 tiles
@@ -25,8 +28,6 @@ class CatanBoard:
         self.total_tiles_per_player = 7.0
         
         # probability config
-        self.roll_lower_bound_pct = 0.75
-        self.roll_upper_bound_pct = 1.2
         # self.roll_prob_lookup -- the probability (multiplied by 36) that the key number will 
         # roll in the game with 2 dice
         self.roll_prob_lookup = {"3":2,"4":3,"5":4,"6":5,"8":5,"9":4,"10":3,"11":2}
@@ -34,7 +35,7 @@ class CatanBoard:
         # self.roll_select_prob -- for map generation, the weighted probability that the number will
         # be selected
         self.roll_select_prob = {"3":1,"4":1,"5":1,"6":1,"8":1,"9":1,"10":1,"11":1}
-        self.non_resource_tile_select_prob = {TileType.ocean:0.65, TileType.desert:0.35}
+        self.non_resource_tile_select_prob = {TileType.ocean:0.75, TileType.desert:0.25}
         if enable_bonus_rolls == True:
             self.roll_prob_lookup.update({"2|12":2,"2|2":2,"12|12":2})
             self.roll_select_prob.update({"2|12":0.5,"2|2":0.5,"12|12":0.5})
@@ -67,22 +68,59 @@ class CatanBoard:
                 random.shuffle(ring_coords)
             hex_coords += ring_coords
         return hex_coords
+    
+    def _gen_bag_of_rtiles(self,n_resource_tiles):
+        '''Create the bag of roll tiles to choose from'''
+        bag_of_rtiles = []
+        remainder_keys = []
+        remainder_vals = []
+        total_shares = sum(self.roll_select_prob.values())
+        for key,value in self.roll_select_prob.items():
 
-    def _gen_rolls(self,n,lower_bound_pct = .75, upper_bound_pct = 1.2):
-        ''' Will generate n numbers (to place on hex tiles) that averages close to the configured target probability.'''
-        roll_options,roll_options_probs = list(self.roll_select_prob.keys()),list(self.roll_select_prob.values())
-        roll_options_probs /= np.sum(roll_options_probs)
-        MAX_ROLLS = 1000
-        i = 0
+            expected_value = value*n_resource_tiles/total_shares
+            bag_of_rtiles += [key] * int(expected_value)
+            remainder_keys.append(key)
+            remainder_vals.append(expected_value % 1)
+
+        n_remaining_rtiles = n_resource_tiles - len(bag_of_rtiles)
+        bag_of_rtiles += list(np.random.choice(remainder_keys,n_remaining_rtiles,p=np.array(remainder_vals)/sum(remainder_vals),replace=False))
+        return bag_of_rtiles
+    
+    def _gen_rtiles(self,bag_of_rtiles,n_each_resource):
+        # generate a set of rtiles for each resource type
+        MAX_TRIES = 30
+        i_loop = 0
+        tiles = []
         while(True):
-            i += 1
-            rolls = np.random.choice(roll_options,n,p=roll_options_probs)
-            mean_prob = np.mean([self.roll_prob_lookup[x] for x in rolls])
-            if mean_prob < self.target_avg_roll_probability * upper_bound_pct and mean_prob > self.target_avg_roll_probability * lower_bound_pct:
+            bag_of_rtiles_copy = bag_of_rtiles.copy()
+            random.shuffle(bag_of_rtiles_copy)
+            rtiles_per_res = []
+            for i in range(len(RESOURCE_TILE_TYPES)):
+                rtiles_per_res.append(bag_of_rtiles_copy[i*n_each_resource:(i+1)*n_each_resource])
+
+            # check to see if it is balanced
+            total_pips = []
+            for rtiles in rtiles_per_res:
+                total_pips.append(sum([self.roll_prob_lookup[x] for x in rtiles]))
+            total_pips = np.array(total_pips)
+
+            x = np.mean(total_pips)
+
+            if not np.any( (total_pips < self.roll_lower_bound_pct*x) | (total_pips > self.roll_upper_bound_pct*x) ):
                 break
-            if i == MAX_ROLLS:
-                raise Exception("Couldn't generate rolls, try expanding the upper and lower bounds or changing the target probability.")
-        return rolls
+                
+            i_loop += 1
+            if i_loop > MAX_TRIES:
+                raise Exception("couldnt generate rtiles")
+        
+        for i in range(len(RESOURCE_TILE_TYPES)):
+            res = RESOURCE_TILE_TYPES[i]
+            rolls = rtiles_per_res[i]
+            for j in range(n_each_resource):
+                    tiles.append(Tile(res,rolls[j]))
+                
+        return tiles
+        
     
     def _gen_tiles(self,n_players):
         ''' generates the tiles of a balanced map'''
@@ -90,13 +128,14 @@ class CatanBoard:
         n_resource_tiles = int(n_each_resource) * len(RESOURCE_TILE_TYPES)
         n_non_resource = int(n_players*self.total_tiles_per_player - n_resource_tiles)
         
+        bag_of_rtiles = self._gen_bag_of_rtiles(n_resource_tiles)
         
-        non_resource_tile_types,non_resource_tile_probs = list(self.non_resource_tile_select_prob.keys()), list(self.non_resource_tile_select_prob.values())
+        
         tiles = []
-        for res in RESOURCE_TILE_TYPES:
-            rolls = self._gen_rolls(n_each_resource,self.roll_lower_bound_pct,self.roll_upper_bound_pct)
-            for i in range(n_each_resource):
-                tiles.append(Tile(res,rolls[i]))
+        
+        tiles += self._gen_rtiles(bag_of_rtiles,n_each_resource)
+
+        non_resource_tile_types,non_resource_tile_probs = list(self.non_resource_tile_select_prob.keys()), list(self.non_resource_tile_select_prob.values()) 
         for i in range(n_non_resource):
             res = np.random.choice(non_resource_tile_types,p=non_resource_tile_probs)
             tiles.append(Tile(res))
@@ -108,7 +147,6 @@ class CatanBoard:
         
         for i in range(n_tiles):
             tiles[i].set_coordinate(*hex_coords[i])
-        
         
             
         return tiles
